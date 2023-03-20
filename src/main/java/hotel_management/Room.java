feb11b759cdc7f3d9afe36ee52787a10a88acb57
Killerbacon34 <127875772+Killerbacon34@users.noreply.github.com>
2023-03-20 15:19:13 -0500

    Update Room.java

--- src/main/java/hotel_management/Room.java
+++ src/main/java/hotel_management/Room.java
@@ -1,12 +1,53 @@
 package hotel_management;
-
 public class Room {
     private enum BedType {
         //todo
+        TWIN,
+        FULL,
+        KING,
+        QUEEN
+    }
+    private enum QualityType {
+        EXECUTIVE,
+        BUSINESS,
+        COMFORT,
+        ECONOMY
     }
     private int id;
     private int numBeds;
     private BedType bedType;
     private boolean canSmoke;
     private int qualityLevel;
+    private QualityType qualityLevel;
+
+    public int getID() {
+        return id;
+    }
+    public void setID(int id) {
+        this.id = id;
+    }
+    public int getNumBeds() {
+        return numBeds;
+    }
+    public void setNumBeds(int numBeds) {
+        this.numBeds = numBeds;
+    }
+    public int getBedType() {
+        return bedType;
+    }
+    public void setBedType(BedType bedType) {
+        this.bedType = bedType;
+    }
+    public boolean getCanSmoke() {
+        return canSmoke;
+    }
+    public void setCanSmoke(boolean canSmoke) {
+        this.canSmoke = canSmoke;
+    }
+    public int getQualityType() {
+        return qualityLevel;
+    }
+    public void setID(QualityType qualityLevel) {
+        this.qualityLevel = qualityLevel;
+    }
 }
