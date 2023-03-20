909708a9c4ccf95f9b1bbdd670116080e8ac2338
Christianocana <90228217+Christianocana@users.noreply.github.com>
2023-03-20 16:32:19 -0500

    Update Billing.java

--- src/main/java/billing_services/Billing.java
+++ src/main/java/billing_services/Billing.java
@@ -1,4 +1,32 @@
 package billing_services;
 
+import hotel_management.*;
+
 public class Billing {
+    private Account user;
+    private Double totalCost;
+    private Double tip;
+    public Billing(Account u, Double tc, Double t) {
+        user = u;
+        totalCost = tc;
+        tip = t;
+    }
+    public void setUser(Account u) {
+        user = u;
+    }
+    public Account getUser() {
+        return user;
+    }
+    public void setTip(Double t) {
+        tip = t;
+    }
+    public Double getTip() {
+        return tip;
+    }
+    public Double getTotalCost() {
+        return totalCost;
+    }
+    public void calculateTotal() {
+        totalCost = 0.0; // need to calculate total of reservation(s) including tax and tip
+    }
 }
