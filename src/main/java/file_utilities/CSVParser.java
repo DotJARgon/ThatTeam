1e27b5577f2411a47cbf738aa049a2bc20cfce8c
unknown <mcar3550@students.kleinisd.net>
2023-03-23 12:36:24 -0500

    created basic csv helper

---
+++ src/main/java/file_utilities/CSVParser.java
@@ -0,0 +1,38 @@
+package file_utilities;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class CSVParser {
+    static ArrayList<String[]> loadCSV(String filename) {
+        try {
+            ArrayList<String[]> allEntries = new ArrayList<>();
+            BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+            String line = "";
+            while((line = br.readLine()) != null) {
+                allEntries.add(line.split(","));
+            }
+            br.close();
+            return allEntries;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    static void writeCSV(String filename, ArrayList<Object[]> allEntries) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File(filename)));
+            for(Object[] line : allEntries) {
+                for(Object o : line) bw.write(o.toString());
+                bw.write('\n');
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
