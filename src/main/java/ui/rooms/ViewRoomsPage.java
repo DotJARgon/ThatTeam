f62ef0edd2da1296fc93129ccaf959c16a3e4015
unknown <mcar3550@students.kleinisd.net>
2023-03-30 22:35:00 -0500

    added beginning of view room

---
+++ src/main/java/ui/rooms/ViewRoomsPage.java
@@ -0,0 +1,11 @@
+package ui.rooms;
+
+import javax.swing.*;
+
+public class ViewRoomsPage extends JPanel {
+    public ViewRoomsPage() {
+        super();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(Box.createHorizontalGlue());
+    }
+}
