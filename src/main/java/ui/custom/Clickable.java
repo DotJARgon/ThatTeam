12b561e08f2ce3c83e48a57b817edebcf40f7a8f
unknown <mcar3550@students.kleinisd.net>
2023-04-29 16:30:05 -0500

    more ui comments

--- src/main/java/ui/custom/Clickable.java
+++ src/main/java/ui/custom/Clickable.java
@@ -1,6 +1,16 @@
 package ui.custom;
 
+/**
+ * Clickable is a functional interface meant to allow for a callback
+ * with the ClickableText class. This is to allow for custom behavior
+ * upon clicking on the ClickableText, and is highly reusable, it
+ * requires no parameters nor needs to return.
+ * */
 @FunctionalInterface
 public interface Clickable {
+    /**
+     * callback is a simple function that takes no parameters and returns
+     * nothing
+     * */
     void callback();
 }
--- src/main/java/ui/custom/NavUpdate.java
+++ src/main/java/ui/custom/NavUpdate.java
@@ -1,5 +1,17 @@
 package ui.custom;
 
+/**
+ * The NavUpdate interface is meant to allow for individual pages
+ * to "know" that they are being viewed, this is to allow the page
+ * to populate data. It keeps resource usage down since there
+ * is no need to constantly update every page constantly.
+ * */
 public interface NavUpdate {
+    /**
+     * navUpdate is an update function called before a navigation
+     * is completed to a UI element, giving the UI element time to
+     * load data (especially useful for classes that cannot initialize,
+     * all data upon being created)
+     * */
     void navUpdate();
 }
